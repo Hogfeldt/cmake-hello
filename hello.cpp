5d374b217c7045a98953165d33147f661ab7ef52
Per Høgfeldt <hogefeldt38@gmail.com>
2019-03-09 15:41:51 +0100

    The hello.cpp will now correctly use a cppunit library

--- hello.cpp
+++ hello.cpp
@@ -1,13 +1,45 @@
-#include <boost/lambda/lambda.hpp>
 #include <iostream>
-#include <iterator>
-#include <algorithm>
 
-int main()
+#include <cppunit/TestRunner.h>
+#include <cppunit/TestResult.h>
+#include <cppunit/TestResultCollector.h>
+#include <cppunit/extensions/HelperMacros.h>
+#include <cppunit/BriefTestProgressListener.h>
+#include <cppunit/extensions/TestFactoryRegistry.h>
+
+class Test : public CPPUNIT_NS::TestCase
 {
-    using namespace boost::lambda;
-    typedef std::istream_iterator<int> in;
+  CPPUNIT_TEST_SUITE(Test);
+  CPPUNIT_TEST(testHelloWorld);
+  CPPUNIT_TEST_SUITE_END();
 
-    std::for_each(
-        in(std::cin), in(), std::cout << (_1 * 3) << " " );
+public:
+  void setUp(void) {}
+  void tearDown(void) {}
+
+protected:
+  void testHelloWorld(void) { std::cout << "Hello, world!" << std::endl; }
+};
+
+CPPUNIT_TEST_SUITE_REGISTRATION(Test);
+
+int main( int ac, char **av )
+{
+  //--- Create the event manager and test controller
+  CPPUNIT_NS::TestResult controller;
+
+  //--- Add a listener that colllects test result
+  CPPUNIT_NS::TestResultCollector result;
+  controller.addListener( &result );
+
+  //--- Add a listener that print dots as test run.
+  CPPUNIT_NS::BriefTestProgressListener progress;
+  controller.addListener( &progress );
+
+  //--- Add the top suite to the test runner
+  CPPUNIT_NS::TestRunner runner;
+  runner.addTest( CPPUNIT_NS::TestFactoryRegistry::getRegistry().makeTest() );
+  runner.run( controller );
+
+  return result.wasSuccessful() ? 0 : 1;
 }
